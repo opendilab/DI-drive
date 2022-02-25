94cd06c4006bebdb8a8460a872afbca199d2b486
chenruobing <chenruobing@sensetime.com>
2022-02-25 13:29:03 +0000

    Merge branch 'dev-pipeline' into 'dev'
    Dev pipeline
    
    See merge request open-XLab/cell/xad!115

--- demo/simple_rl/simple_rl_train.py
+++ demo/simple_rl/simple_rl_train.py
@@ -14,6 +14,8 @@ from ding.policy import DQNPolicy, PPOPolicy, TD3Policy, SACPolicy, DDPGPolicy
 from ding.worker import BaseLearner, SampleSerialCollector, AdvancedReplayBuffer, NaiveReplayBuffer
 from ding.utils import set_pkg_seed
 from ding.rl_utils import get_epsilon_greedy_fn
+from ding.framework import Task
+from ding.framework.wrapper import StepTimer
 
 from demo.simple_rl.model import DQNRLModel, PPORLModel, TD3RLModel, SACRLModel, DDPGRLModel
 from demo.simple_rl.env_wrapper import DiscreteEnvWrapper, ContinuousEnvWrapper
@@ -46,7 +48,13 @@ def get_cfg(args):
     default_train_config = read_ding_config(ding_cfg)
     default_train_config.exp_name = args.name
     use_policy, _ = get_cls(args.policy)
-    use_buffer = AdvancedReplayBuffer if args.policy != 'ppo' else None
+    use_buffer = {
+        'dqn': AdvancedReplayBuffer,
+        'ppo': None,
+        'td3': NaiveReplayBuffer,
+        'sac': NaiveReplayBuffer,
+        'ddpg': NaiveReplayBuffer,
+    }[args.policy]
     cfg = compile_config(
         cfg = default_train_config,
         env_manager = SyncSubprocessEnvManager,
@@ -68,6 +76,60 @@ def get_cls(spec):
 
     return policy_cls, model_cls
 
+def evaluate(task, evaluator, learner):
+    def _evaluate(ctx):
+        ctx.setdefault("envstep", -1)  # Avoid attribute not existing
+        ctx.setdefault("train_iter", -1)
+        if evaluator.should_eval(ctx.train_iter):
+            stop, rate = evaluator.eval(learner.save_checkpoint, learner.train_iter, ctx.envstep)
+            if stop:
+                task.finish = True
+                return
+    return _evaluate
+
+
+def off_policy_collect(epsilon_greedy, collector, replay_buffer, cfg):
+    def _collect(ctx):
+        ctx.setdefault("train_iter", -1)
+        if epsilon_greedy is not None:
+            eps = epsilon_greedy(collector.envstep)
+            new_data = collector.collect(train_iter=ctx.train_iter, policy_kwargs={'eps': eps})
+        else:
+            new_data = collector.collect(train_iter=ctx.train_iter)
+        ctx.update_per_collect = len(new_data) // cfg.policy.learn.batch_size * 4
+        replay_buffer.push(new_data, cur_collector_envstep=collector.envstep)
+    return _collect
+
+
+def on_policy_collect(collector):
+    def _collect(ctx):
+        ctx.setdefault("train_iter", -1)
+        new_data = collector.collect(n_sample=3000, train_iter=ctx.train_iter)
+        unpack_birdview(new_data)
+        ctx.new_data = new_data
+    return _collect
+
+
+def train(learner, replay_buffer, cfg):
+    def _train(ctx):
+        ctx.setdefault("envstep", -1)
+        if 'new_data' in ctx:
+            learner.train(ctx.new_data, ctx.envstep)
+        else:
+            if 'update_per_collect' in ctx:
+                update_per_collect = ctx.update_per_collect
+            else:
+                update_per_collect = cfg.policy.learn.update_per_collect
+            for i in range(update_per_collect):
+                train_data = replay_buffer.sample(cfg.policy.learn.batch_size, learner.train_iter)
+                if train_data is not None:
+                    train_data = copy.deepcopy(train_data)
+                    unpack_birdview(train_data)
+                    learner.train(train_data, ctx.envstep)
+                if cfg.policy.get('priority', False):
+                    replay_buffer.update(learner.priority_info)
+    return _train
+
 
 def main(args, seed=0):
     cfg = get_cfg(args)
@@ -83,12 +145,11 @@ def main(args, seed=0):
     else:
         wrapped_env = wrapped_continuous_env
 
-
     collector_env = SyncSubprocessEnvManager(
         env_fn=[partial(wrapped_env, cfg.env, cfg.env.wrapper.collect, *tcp_list[i]) for i in range(collector_env_num)],
         cfg=cfg.env.manager.collect,
     )
-    evaluate_env = BaseEnvManager(
+    evaluate_env = SyncSubprocessEnvManager(
         env_fn=[partial(wrapped_env, cfg.env, cfg.env.wrapper.eval, *tcp_list[collector_env_num + i]) for i in range(evaluator_env_num)],
         cfg=cfg.env.manager.eval,
         )
@@ -108,49 +169,39 @@ def main(args, seed=0):
     collector = SampleSerialCollector(cfg.policy.collect.collector, collector_env, policy.collect_mode, tb_logger, exp_name=cfg.exp_name)
     evaluator = SerialEvaluator(cfg.policy.eval.evaluator, evaluate_env, policy.eval_mode, tb_logger, exp_name=cfg.exp_name)
 
-    if cfg.policy.get('priority', False):
-        replay_buffer = AdvancedReplayBuffer(cfg.policy.other.replay_buffer, tb_logger, exp_name=cfg.exp_name)
+    if args.policy != 'ppo':
+        if cfg.policy.get('priority', False):
+            replay_buffer = AdvancedReplayBuffer(cfg.policy.other.replay_buffer, tb_logger, exp_name=cfg.exp_name)
+        else:
+            replay_buffer = NaiveReplayBuffer(cfg.policy.other.replay_buffer, tb_logger, exp_name=cfg.exp_name)
     else:
-        replay_buffer = NaiveReplayBuffer(cfg.policy.other.replay_buffer, tb_logger, exp_name=cfg.exp_name)
+        replay_buffer = None
 
     if args.policy == 'dqn':
         eps_cfg = cfg.policy.other.eps
         epsilon_greedy = get_epsilon_greedy_fn(eps_cfg.start, eps_cfg.end, eps_cfg.decay, eps_cfg.type)
+    else:
+        epsilon_greedy = None
 
     learner.call_hook('before_run')
 
-    if args.policy != 'ppo':
-        if args.policy == 'dqn':
+    if cfg.policy.get('random_collect_size', 0) > 0:
+        if epsilon_greedy is not None:
             eps = epsilon_greedy(collector.envstep)
-            new_data = collector.collect(n_sample=10000, train_iter=learner.train_iter, policy_kwargs={'eps': eps})
+            new_data = collector.collect(n_sample=cfg.policy.random_collect_size, policy_kwargs={'eps': eps})
         else:
-            new_data = collector.collect(n_sample=10000, train_iter=learner.train_iter)
+            new_data = collector.collect(n_sample=cfg.policy.random_collect_size)
         replay_buffer.push(new_data, cur_collector_envstep=collector.envstep)
-
-    while True:
-        if evaluator.should_eval(learner.train_iter):
-            stop, rate = evaluator.eval(learner.save_checkpoint, learner.train_iter, collector.envstep)
-            if stop:
-                break
-        if args.policy == 'dqn':
-            eps = epsilon_greedy(collector.envstep)
-            new_data = collector.collect(train_iter=learner.train_iter, policy_kwargs={'eps': eps})
+    
+    with Task(async_mode=args.use_async) as task:
+        task.use_step_wrapper(StepTimer(print_per_step=1)) 
+        task.use(evaluate(task, evaluator, learner))
+        if replay_buffer is None:
+            task.use(on_policy_collect(collector))
         else:
-            new_data = collector.collect(train_iter=learner.train_iter)
-        unpack_birdview(new_data)
-        if args.policy == 'ppo':
-            learner.train(new_data, collector.envstep)
-        else:
-            update_per_collect = len(new_data) // cfg.policy.learn.batch_size * 4
-            replay_buffer.push(new_data, cur_collector_envstep=collector.envstep)
-            for i in range(update_per_collect):
-                train_data = replay_buffer.sample(cfg.policy.learn.batch_size, learner.train_iter)
-                if train_data is not None:
-                    train_data = copy.deepcopy(train_data)
-                    unpack_birdview(train_data)
-                    learner.train(train_data, collector.envstep)
-                if args.policy == 'dqn':
-                    replay_buffer.update(learner.priority_info)
+            task.use(off_policy_collect(epsilon_greedy, collector, replay_buffer, cfg))
+        task.use(train(learner, replay_buffer, cfg))
+        task.run(max_step=int(1e8))
     
     learner.call_hook('after_run')
 
@@ -168,6 +219,7 @@ if __name__ == "__main__":
     parser.add_argument('-n', '--name', type=str, default='simple-rl', help='experiment name')
     parser.add_argument('-p', '--policy', default='dqn', choices=['dqn', 'ppo', 'td3', 'sac', 'ddpg'], help='RL policy')
     parser.add_argument('-d', '--ding-cfg', default=None, help='DI-engine config path')
+    parser.add_argument('--use-async', action='store_true', help='whether use asynchronous execution mode')
     
     args = parser.parse_args()
     main(args)
